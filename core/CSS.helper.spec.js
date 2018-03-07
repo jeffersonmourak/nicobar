8d93bddc86bca3c83fce2d891b66b281ee2683cb
jefferson oliveira <jefinho.moura14@gmail.com>
2018-03-07 11:33:53 -0300

    updating tests for css

--- core/CSS.helper.spec.js
+++ core/CSS.helper.spec.js
@@ -1,88 +1,88 @@
-// import Css from './CSS.helper';
-// import _ from 'lodash';
+import CSS from './CSS.helper';
+import _ from 'lodash';
 
-// describe('CSS.helper.js', () => {
-//   it('should exists', () => {
-//     expect(Css).toBeTruthy();
-//   });
+describe('CSS.helper.js', () => {
+  it('should exists', () => {
+    expect(CSS).toBeTruthy();
+  });
 
-//   describe('getClassHash()', () => {
-//     it('should exists', () => {
-//       expect(Css.getClassHash).toBeTruthy();
-//     });
+  describe('getClassHash()', () => {
+    it('should exists', () => {
+      expect(CSS.getClassHash).toBeTruthy();
+    });
 
-//     it('should return a random string with `nicobar-` prefix', () => {
-//       expect(_.startsWith(Css.getClassHash(), 'nicobar-')).toBe(true);
-//     })
-//   });
+    it('should return a random string with `nicobar-` prefix', () => {
+      expect(_.startsWith(CSS.getClassHash(), 'nicobar-')).toBe(true);
+    })
+  });
 
-//   describe('hasNicobar(element)', () => {
-//     let element;
+  describe('hasNicobar(element)', () => {
+    let element;
 
-//     beforeEach( () => {
-//       element = {
-//         className: 'test'
-//       };
-//     } );
+    beforeEach( () => {
+      element = {
+        className: 'test'
+      };
+    } );
 
-//     it('should exists', () => {
-//       expect(Css.hasNicobar).toBeTruthy();
-//     });
+    it('should exists', () => {
+      expect(CSS.hasNicobar).toBeTruthy();
+    });
 
-//     it('should return nicobar index', () => {
-//       element.className = 'something nicobar-test';
-//       expect(Css.hasNicobar(element)).toBe('nicobar-test');
-//     });
+    it('should return nicobar index', () => {
+      element.className = 'something nicobar-test';
+      expect(CSS.hasNicobar(element)).toBe('nicobar-test');
+    });
 
-//     it('should return undefined when not exists', () => {
-//       element.className = 'something';
-//       expect(Css.hasNicobar(element)).toBe(undefined);
-//     });
-//   });
+    it('should return undefined when not exists', () => {
+      element.className = 'something';
+      expect(CSS.hasNicobar(element)).toBe(undefined);
+    });
+  });
 
-//   describe('intelligentSelector(element)', () => {
-//     let element;
+  describe('intelligentSelector(element)', () => {
+    let element;
 
-//     beforeEach( () => {
-//       element = {
-//         className: 'test'
-//       };
-//     } );
+    beforeEach( () => {
+      element = {
+        className: 'test'
+      };
+    } );
 
-//     it('should exists', () => {
-//       expect(Css.intelligentSelector).toBeTruthy();
-//     });
+    it('should exists', () => {
+      expect(CSS.intelligentSelector).toBeTruthy();
+    });
 
-//     it('should return id', () => {
-//       element.id = 'id';
-//       expect(Css.intelligentSelector(element)).toBe('#id');
-//     });
+    it('should return id', () => {
+      element.id = 'id';
+      expect(CSS.intelligentSelector(element)).toBe('#id');
+    });
 
-//     it('should return nicobar class', () => {
-//       element.className = 'nicobar-test';
-//       expect(Css.intelligentSelector(element)).toBe('.nicobar-test');
-//     });
+    it('should return nicobar class', () => {
+      element.className = 'nicobar-test';
+      expect(CSS.intelligentSelector(element)).toBe('.nicobar-test');
+    });
 
-//     it('should return a random nicobar class', () => {
-//       let className = Css.intelligentSelector(element);
-//       expect(_.startsWith(className, '.nicobar-')).toBe(true);
-//     });
-//   });
+    it('should return a random nicobar class', () => {
+      let className = CSS.intelligentSelector(element);
+      expect(_.startsWith(className, '.nicobar-')).toBe(true);
+    });
+  });
 
-//   describe('fromObjToCss(object)', () => {
+  describe('fromObjToCSS(object)', () => {
 
-//     it('should exists', () => {
-//       expect(Css.fromObjToCss).toBeTruthy();
-//     });
+    it('should exists', () => {
+      expect(CSS.fromObjToCss).toBeTruthy();
+    });
 
-//     it('should return the Css', () => {
-//       let obj = {
-//         property: 'value',
-//         property2: 'value2',
-//         property3: 'value3',
-//       },
-//       str = '--property: value;--property2: value2;--property3: value3;';
-//       expect(Css.fromObjToCss(obj)).toBe(str);
-//     });
-//   });
-// });
+    it('should return the CSS', () => {
+      let obj = {
+        property: 'value',
+        property2: 'value2',
+        property3: 'value3',
+      },
+      str = '--property: value;--property2: value2;--property3: value3;';
+      expect(CSS.fromObjToCss(obj)).toBe(str);
+    });
+  });
+});
